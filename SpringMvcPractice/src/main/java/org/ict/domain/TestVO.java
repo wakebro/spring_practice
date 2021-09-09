053241ec659f7d902524075c49cfcdb472caeb60
DESKTOP-UV9647E\fbrk0 <wakebro119@naver.com>
2021-09-09 11:02:13 +0900

    20210909_set lombok

---
+++ SpringMvcPractice/src/main/java/org/ict/domain/TestVO.java
@@ -0,0 +1,12 @@
+package org.ict.domain;
+
+import lombok.Data;
+
+// lombok을 이용하면 자동으로 getter, setter, toString을 생성
+// 		좌측 Pcakage Explporer 생성한 클래스의 하단에서 확인
+// @Data 어노테이션을 클래스 위에 붙이면 자동생성된다.
+@Data
+public class TestVO {
+	private String name;
+	private int age;
+}
