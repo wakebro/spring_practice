6d05b44f74f4cba2febdcd711f826920ae32da43
DESKTOP-UV9647E\fbrk0 <wakebro119@naver.com>
2021-09-21 18:37:52 +0900

    20210921_스프링 기초 복습

---
+++ PersnalSpring/src/main/java/com/hgs/mapper/TimeMapper.java
@@ -0,0 +1,12 @@
+package com.hgs.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+	
+	@Select("SELECT SYSDATE FROM DUAL")
+	public String getTime();
+	
+	public String getTime2();
+	
+}
