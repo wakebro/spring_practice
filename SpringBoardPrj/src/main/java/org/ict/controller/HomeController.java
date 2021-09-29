e3712f341c47002f31dad55111bfcd970838cf04
wake <wakebro119@naver.com>
2021-09-29 13:13:39 +0900

    20210929_REST 환경설정

--- SpringBoardPrj/src/main/java/org/ict/controller/HomeController.java
+++ SpringBoardPrj/src/main/java/org/ict/controller/HomeController.java
@@ -4,17 +4,25 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import jdk.internal.org.jline.utils.Log;
+import lombok.extern.log4j.Log4j;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@Log4j
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -34,6 +42,31 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	// 세션 추가를 위해서 컨트롤러 내부 메서드마다 설정을 해주어야 한다_비효율적
+	// HttpServletRequest 타입 변수 선언하고
+	// 내부에 .getSession() 명령어로 세션을 받아와 사용
+	@GetMapping("/session1")
+	public String se1(HttpServletRequest request) {
+		// 위와 같이 HttpServletRequest 객체를 파라미터에 선언 후
+		// HttpSession 객체를 얻어오면 세션기능을 그대로 사용 가능하다.
+		HttpSession session = request.getSession();
+		
+		session.setAttribute("sTest", "123");
+		
+		return "sesseion1";
+	}
+	
+	@GetMapping("/session2")
+	public String se2(HttpServletRequest request) {
+		// 위와 같이 HttpServletRequest 객체를 파라미터로 선언한 후,
+		// HttpSession 객체를 얻어오면 세션 기능을 쓸 수 있다.
+		HttpSession session = request.getSession();
+		
+		log.info("세션작동확인 : " + session.getAttribute("sTest"));
+		
+		return "";
 	}
 	
 }
--- SpringBoardPrj/src/main/webapp/WEB-INF/views/board/modify.jsp
+++ SpringBoardPrj/src/main/webapp/WEB-INF/views/board/modify.jsp
@@ -8,8 +8,19 @@
 </head>
 <body>
 	<h1>${vo.bno }번 글 수정 페이지</h1>
+	페이지번호 : ${param.pageNum}<br>
+	검색조건: ${param.searchType }<br>
+	키워드 : ${param.keyword}<br>
+	<!-- 수정을 하면서 동시에 페이지번호, 검색조건, 키워드를 넘긴다 
+	수정 로직은 post이기 때문에 어쩔수 없이 컨트롤러를 경유해서 데이터를 전달한다.
+	Controller 내부의 modify메서드에 의해 위 3가지 정보를 모두 처리할 수 있는
+	SearchCriteria를 추가로 선언하여 받아준다.
+	-->
 	<form action="/board/modify" method="post">
 		<input type="hidden" name="bno" value="${vo.bno }"><br>
+		<input type="hidden" name="pageNum" value="${param.pageNum}">
+		<input type="hidden" name="searchType" value="${param.searchType}">
+		<input type="hidden" name="keyword" value="${param.keyword}">
 		제목 : <input type="text" name="title" value=" ${vo.title}"><br>
 		본문 : <br>
 		<textarea name="content"  rows="10" cols="50">${vo.content}</textarea><br>
@@ -20,8 +31,6 @@
 		<input type="submit" value="확인">
 	</form>
 	<a href="/board/list"><button>목록</button></a><br>
-	페이지번호 : ${param.pageNum}<br>
-	검색조건: ${param.searchType }<br>
-	키워드 : ${param.keyword}<br>
+
 </body>
 </html>
