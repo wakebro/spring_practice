1769830983db7ba6907acaba2b2aa08544694eb0
wake <wakebro119@naver.com>
2021-10-13 10:41:20 +0900

    20211013_파일 업로드 세팅

---
+++ prj_upload/src/main/java/org/ict/controller/UploadController.java
@@ -0,0 +1,24 @@
+package org.ict.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class UploadController {
+	
+	@GetMapping("/uploadForm")
+	public void uploadForm() {
+		log.info("upload form");
+	}
+	
+	@PostMapping("/uploadFormAction")
+	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+		
+	}
+}
